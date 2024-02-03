d89db589e5caa7578239a1b05641fe3caa42f6dc
Artem <postaluka07@gmail.com>
2024-02-03 12:24:13 +0200

    reset

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
-import Inspect from 'vite-plugin-inspect'
-import { resolve } from 'path'
-
 export default {
     root: 'src/',
     publicDir: '../static/',
@@ -16,13 +13,6 @@ export default {
     {
         outDir: '../dist',
         emptyOutDir: true,
-        sourcemap: true,
-        rollupOptions:
-        {
-            input: {
-                main: resolve(__dirname, 'src/index.html'),
-                lesson01: resolve(__dirname, 'src/lesson01/index.html')
-            }
-        }
+        sourcemap: true
     }
 }
