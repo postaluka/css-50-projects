751bb3ce93885694496d31f743fc57c53d9236a8
Artem <postaluka07@gmail.com>
2024-02-05 12:39:11 +0200

    lesson 03 done

--- src/Lessons/03/lesson03.js
+++ src/Lessons/03/lesson03.js
@@ -1,73 +1,14 @@
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const progress = document.getElementById('progress')
-const circles = document.querySelectorAll('.circle')
+const open = document.getElementById('open')
+const close = document.getElementById('close')
 
-let activeNumber = 1
+const container = document.querySelector('.container')
 
-next.addEventListener('click', () =>
+open.addEventListener('click', () =>
 {
-    activeNumber++
-    if (activeNumber > circles.length)
-    {
-        activeNumber = circles.length
-    }
-
-    update()
-})
-prev.addEventListener('click', () =>
-{
-    activeNumber--
-    if (activeNumber < 1)
-    {
-        activeNumber = 1
-    }
-
-    update()
+    container.classList.add('show-nav')
 })
 
-function update()
+close.addEventListener('click', () =>
 {
-    circles.forEach((circle, index) =>
-    {
-        if (index < activeNumber)
-        {
-            circle.classList.add('active')
-
-        } else
-        {
-            circle.classList.remove('active')
-        }
-    })
-
-    const length = (activeNumber - 1) / (circles.length - 1) * 100 + "%"
-
-    console.log(length);
-
-    progress.style.width = length
-
-
-    if (activeNumber === 1)
-    {
-        prev.disabled = true
-    }
-    else if (activeNumber === circles.length)
-    {
-        next.disabled = true
-    }
-    else
-    {
-        prev.disabled = false
-        next.disabled = false
-    }
-}
-
-
-/**
- * Canvas
- */
-const canvas = document.querySelector('canvas.webgl')
-
-
-
-
+    container.classList.remove('show-nav')
+})
